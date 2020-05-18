7daab496e2bd0165b90e5762124e89599c41daef
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-05-18 13:18:46 -0300

    Initial Commit

---
+++ FundamentosArquitetura/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FundamentosArquitetura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
