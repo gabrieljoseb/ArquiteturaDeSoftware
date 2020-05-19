b76db22fca7f824a80e427b53264f4f8d61f8c5f
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-05-19 16:36:31 -0300

    02 - Pilares OOP: 03 - Abstracao.cs

---
+++ FundamentosArquitetura/02 - Pilares OOP/03 - Abstracao.cs
@@ -0,0 +1,18 @@
+﻿namespace OOP
+{
+    // A classe só pode ser herdada e nunca instanciada
+    public abstract class Eletrodomestico
+    {
+        private readonly string _nome;
+        private readonly int _voltagem;
+
+        protected Eletrodomestico(string nome, int voltagem)
+        {
+            _nome = nome;
+            _voltagem = voltagem;
+        }
+
+        public abstract void Ligar();
+        public abstract void Desligar();
+    }
+}
