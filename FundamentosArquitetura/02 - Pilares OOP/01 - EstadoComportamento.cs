f3699a87a668dfa292102875855ab09b736c02cd
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-05-19 16:11:45 -0300

    02 - Pilares OOP: 01 - EstadoComportamento.cs

---
+++ FundamentosArquitetura/02 - Pilares OOP/01 - EstadoComportamento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public int CalcularIdade()
+        {
+            var dataAtual = DateTime.Now;
+            var idade = dataAtual.Year - DataNascimento.Year;
+
+            if (dataAtual < DataNascimento.AddYears(idade)) idade--;
+
+            return idade;
+        }
+    }
+}
