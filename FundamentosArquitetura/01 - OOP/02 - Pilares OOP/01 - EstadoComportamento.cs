157601e76f76e4eb68b83fba60744319216da324
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-05-21 12:14:41 -0300

    Ajustes

---
+++ FundamentosArquitetura/01 - OOP/02 - Pilares OOP/01 - EstadoComportamento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OOP
+{
+    public class Pessoa
+    {
+        // Estados
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        // Comportamento
+        public int CalcularIdade()
+        {
+            var dataAtual = DateTime.Now;
+            var idade = dataAtual.Year - DataNascimento.Year;
+
+            if (dataAtual < DataNascimento.AddYears(idade)) idade--;
+
+            return idade;
+        }
+    }
+}
---
+++ FundamentosArquitetura/01 - OOP/02 - Pilares OOP/05 - Encapsulamento.cs
@@ -0,0 +1,13 @@
+﻿namespace OOP
+{
+    class AutomacaoCafe
+    {
+        public void ServirCafe()
+        {
+            var espresso = new CafeteiraEspressa();
+            espresso.Ligar();
+            espresso.PrepararCafe();
+            espresso.Desligar();
+        }
+    }
+}
